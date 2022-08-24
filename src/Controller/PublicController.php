3696824242a0f0997e06f6c50e8501f1d2188e2a
maximilien137 <105204121+maximilien137@users.noreply.github.com>
2022-08-24 14:14:52 +0200

    Loads of commits including jquery and bootstrap uploads

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Messages;
+use App\Form\MessagesType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PublicController extends AbstractController
@@ -25,10 +29,40 @@ class PublicController extends AbstractController
     }
 
     #[Route('/contact', name: 'contact')]
-    public function contact(): Response
+    public function contact(Request $request, EntityManagerInterface $entityManager)
     {
-        return $this->render('public/whoami.html.twig', [
-            'controller_name' => 'Who am I?',
+        $messages = new Messages();
+        
+        $messageFormulaire = $this->createForm(MessagesType::class, $messages);
+        // dd($contactFormulaire);
+        $messageFormulaire->handleRequest($request);
+
+        // dd($contactFormulaire);
+
+        if ($messageFormulaire->isSubmitted() && $messageFormulaire->isValid()) {
+            $entityManager->persist($messages);
+            $entityManager->flush();
+            return new Response('Ayé ENFIN!!!');
+        }
+
+        return $this->render('public/contact.html.twig', [
+            'controller_name' => 'Formulaire de contact qui marche',
+            'frmContact' => $messageFormulaire->createView()
         ]);
+
+
+    }
+
+    #[Route('/message', name: 'message')]
+    public function message(Request $request, EntityManagerInterface $entityManager) {
+        // La méthode findBy() permet d'affiner le résultat des requêtes
+        // $resultat = $entityManager->getRepository(Messages::class)->findBy([
+        //     'id' => [2, 3], // Récupérer les éléments qui ont l'id 2 et 3
+        //     'nom' => 'DESC' // Les classer par nom DESC
+        // ]);
+        $resultat = $entityManager->getRepository(Messages::class)->findAll();
+
+        return $this->render('public/message.html.twig', [
+            'messages' => $resultat]);
     }
 }
---
+++ src/Entity/Messages.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessagesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MessagesRepository::class)]
+class Messages
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Prenom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Email = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Entreprise = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $Telephone = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Message = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->Prenom;
+    }
+
+    public function setPrenom(string $Prenom): self
+    {
+        $this->Prenom = $Prenom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getEntreprise(): ?string
+    {
+        return $this->Entreprise;
+    }
+
+    public function setEntreprise(?string $Entreprise): self
+    {
+        $this->Entreprise = $Entreprise;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->Telephone;
+    }
+
+    public function setTelephone(string $Telephone): self
+    {
+        $this->Telephone = $Telephone;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->Message;
+    }
+
+    public function setMessage(string $Message): self
+    {
+        $this->Message = $Message;
+
+        return $this;
+    }
+}
